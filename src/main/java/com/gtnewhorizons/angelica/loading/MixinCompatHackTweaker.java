c7e97b873e94d03557b262cf96979eb9a87cbf76
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-11-30 23:25:43 +0100

    Remove optimizationsandtweaks remover in MixinCompatHackTweaker
    + fix concurrentmodificationexception in MixinCompatHackTweaker

--- src/main/java/com/gtnewhorizons/angelica/loading/MixinCompatHackTweaker.java
+++ src/main/java/com/gtnewhorizons/angelica/loading/MixinCompatHackTweaker.java
@@ -47,24 +47,32 @@ public class MixinCompatHackTweaker implements ITweaker {
         }
     }
 
-    private void disableXaerosMinimapWaypointTransformer(){
+    private void disableXaerosMinimapWaypointTransformer() {
         try {
             final LaunchClassLoader lcl = Launch.classLoader;
             final Field xformersField = lcl.getClass().getDeclaredField("transformers");
             xformersField.setAccessible(true);
             @SuppressWarnings("unchecked")
             List<IClassTransformer> xformers = (List<IClassTransformer>) xformersField.get(lcl);
-            for (int idx = xformers.size() - 1; idx >= 0; idx--) {
-                final String name = xformers.get(idx).getClass().getName();
+
+            // Use a temporary list to store transformers to remove
+            List<IClassTransformer> toRemove = new ArrayList<>();
+            for (IClassTransformer transformer : xformers) {
+                final String name = transformer.getClass().getName();
                 if (name.startsWith("xaero.common.core.transformer.GuiIngameForgeTransformer")) {
-                    LOGGER.info("Removing transformer " + name);
-                    xformers.remove(idx);
+                    LOGGER.info("Marking transformer for removal: " + name);
+                    toRemove.add(transformer);
                 }
             }
+
+            // Remove transformers outside of iteration
+            xformers.removeAll(toRemove);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
 
     @SuppressWarnings("unchecked")
     private void disableIncompatibleMods() {
@@ -74,81 +82,48 @@ public class MixinCompatHackTweaker implements ITweaker {
             final Field xformersField = lcl.getClass().getDeclaredField("transformers");
             xformersField.setAccessible(true);
             final List<IClassTransformer> xformers = (List<IClassTransformer>) xformersField.get(lcl);
-            for (int idx = xformers.size() - 1; idx >= 0; idx--) {
-                final String name = xformers.get(idx).getClass().getName();
-                if (name.startsWith("optifine") || name.startsWith("fastcraft") || name.startsWith("me.guichaguri.betterfps")) {
-                    LOGGER.info("Removing transformer " + name);
-                    xformers.remove(idx);
-                }
-            }
+            xformers.removeIf(xformer -> xformer.getClass().getName().startsWith("optifine")
+                || xformer.getClass().getName().startsWith("fastcraft")
+                || xformer.getClass().getName().startsWith("me.guichaguri.betterfps"));
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         try {
             final Field injectedContainersField = Loader.class.getDeclaredField("injectedContainers");
             injectedContainersField.setAccessible(true);
-            final List<String> containers = (List<String> ) injectedContainersField.get(Loader.class);
-            for (int idx = containers.size() - 1; idx >= 0; idx--) {
-            final String name = containers.get(idx);
-                if (name.startsWith("optifine") || name.startsWith("fastcraft")) {
-                    LOGGER.info("Removing mod container " + name);
-                    containers.remove(idx);
-                }
-            }
+            final List<String> containers = (List<String>) injectedContainersField.get(Loader.class);
+            containers.removeIf(container -> container.startsWith("optifine") || container.startsWith("fastcraft"));
 
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
             final Field reparsedCoremodsField = CoreModManager.class.getDeclaredField("reparsedCoremods");
             final Field loadedCoremodsField = CoreModManager.class.getDeclaredField("loadedCoremods");
             reparsedCoremodsField.setAccessible(true);
             loadedCoremodsField.setAccessible(true);
-            final ArrayList<String> reparsedCoremods = (ArrayList<String>) reparsedCoremodsField.get(CoreModManager.class);
-            final ArrayList<String> loadedCoremods = (ArrayList<String>) loadedCoremodsField.get(CoreModManager.class);
-            for (int idx = reparsedCoremods.size() - 1; idx >= 0; idx--) {
-                final String coreMod = reparsedCoremods.get(idx);
-                if (coreMod.startsWith("optimizationsandtweaks")) {
-                    LOGGER.info("Removing reparsed coremod " + coreMod);
-                    // Fool the CoreModManager into not checking for a mod container again later
-                    loadedCoremods.add(reparsedCoremods.remove(idx));
-
-                }
-            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         try {
             final ArrayList<String> mixinConfigsDefault = (ArrayList<String>) Launch.blackboard.get("mixin.configs.default");
             if (mixinConfigsDefault != null) {
-                for (int idx = mixinConfigsDefault.size() - 1; idx >= 0; idx--) {
-                    final String name = mixinConfigsDefault.get(idx);
-                    if (name != null && name.contains("optimizationsandtweaks")) {
-                        LOGGER.info("Removing mixin config " + name);
-                        mixinConfigsDefault.remove(idx);
-                    }
-                }
+                mixinConfigsDefault.removeIf(name -> name != null && (name.startsWith("optifine") || name.startsWith("fastcraft")));
             }
             final Set<Config> mixinConfigs = (Set<Config>) Launch.blackboard.get("mixin.configs.queue");
-            final Set<Config> toRemove = new HashSet<>();
             if (mixinConfigs != null) {
-                for (Config config : mixinConfigs) {
-                    final String name = config.getName();
-                    if (name != null && name.contains("optimizationsandtweaks")) {
-                        LOGGER.info("Removing queued mixin config " + config.getName());
-                        toRemove.add(config);
-                    }
-                }
-                mixinConfigs.removeAll(toRemove);
+                mixinConfigs.removeIf(config -> config.getName() != null && (config.getName().startsWith("optifine") || config.getName().startsWith("fastcraft")));
             }
             final MixinPlatformManager platformManager = (MixinPlatformManager) Launch.blackboard.get("mixin.platform");
             if (platformManager != null) {
                 final Field containersField = platformManager.getClass().getDeclaredField("containers");
                 containersField.setAccessible(true);
                 final Map<IContainerHandle, MixinContainer> containers = (Map<IContainerHandle, MixinContainer>) containersField.get(platformManager);
-                for (Map.Entry<IContainerHandle, MixinContainer> entry : containers.entrySet()) {
+                containers.entrySet().removeIf(entry -> {
                     final String attribute = entry.getKey().getAttribute("MixinConfigs");
-                    if(attribute != null && attribute.contains("optimizationsandtweaks")) {
-                        LOGGER.info("Removing mixin container " + entry.getKey().toString());
-                        containers.remove(entry.getKey());
-                    }
-                }
+                    return attribute != null && (attribute.startsWith("optifine") || attribute.startsWith("fastcraft"));
+                });
             }
 
         } catch (Exception e) {
