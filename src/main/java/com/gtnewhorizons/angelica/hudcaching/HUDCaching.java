7efac7d73db71c716cbfbe57b330ad14ec5b0dd4
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-12-01 02:35:47 +0100

    Fix Fix Invalid framebuffer operation from Hud Caching

--- src/main/java/com/gtnewhorizons/angelica/hudcaching/HUDCaching.java
+++ src/main/java/com/gtnewhorizons/angelica/hudcaching/HUDCaching.java
@@ -11,6 +11,7 @@ import com.gtnewhorizons.angelica.mixins.interfaces.GuiIngameForgeAccessor;
 import com.gtnewhorizons.angelica.mixins.interfaces.RenderGameOverlayEventAccessor;
 import com.kentington.thaumichorizons.common.ThaumicHorizons;
 import cpw.mods.fml.common.eventhandler.EventPriority;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import net.dries007.holoInventory.client.Renderer;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.world.WorldEvent;
@@ -71,8 +72,24 @@ public class HUDCaching {
     public void onJoinWorld(WorldEvent.Load event) {
         if (event.world.isRemote){
             LOGGER.info("World loaded - Initializing HUDCaching");
-            HUDCaching.framebuffer = new Framebuffer(0, 0, true);
+            int framebufferWidth = Minecraft.getMinecraft().displayWidth;
+            int framebufferHeight = Minecraft.getMinecraft().displayHeight;
+            HUDCaching.framebuffer = new Framebuffer(framebufferWidth, framebufferHeight, true);
             HUDCaching.framebuffer.setFramebufferColor(0, 0, 0, 0);
+        }
+    }
+
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            int framebufferWidth = Minecraft.getMinecraft().displayWidth;
+            int framebufferHeight = Minecraft.getMinecraft().displayHeight;
+
+            if (framebuffer != null && (framebuffer.framebufferWidth != framebufferWidth || framebuffer.framebufferHeight != framebufferHeight)) {
+                framebuffer.createBindFramebuffer(framebufferWidth, framebufferHeight);
+                framebuffer.setFramebufferFilter(GL11.GL_NEAREST);
+                //LOGGER.info("Updated Framebuffer dimensions - Width: {}, Height: {}", framebufferWidth, framebufferHeight);
+            }
         }
     }
 
@@ -188,10 +205,10 @@ public class HUDCaching {
 
     private static void resetFramebuffer(int width, int height) {
         if (framebuffer.framebufferWidth != width || framebuffer.framebufferHeight != height) {
-        	framebuffer.createBindFramebuffer(width, height);
+            framebuffer.createBindFramebuffer(width, height);
             framebuffer.setFramebufferFilter(GL11.GL_NEAREST);
         } else {
-        	framebuffer.framebufferClear();
+            framebuffer.framebufferClear();
         }
         // copy depth buffer from MC
         OpenGlHelper.func_153171_g(GL30.GL_READ_FRAMEBUFFER, mc.getFramebuffer().framebufferObject);
